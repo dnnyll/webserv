e466b88c9a7a686c42c15449e52e3c2bafd11796
dnnyll <leal47danny@gmail.com>
2026-05-28 14:38:49 +0200

    daniefe2 - 2026-05-28 14h38m37 - httpRequest and HttpResponse updated

--- include/HttpRequest.hpp
+++ include/HttpRequest.hpp
@@ -17,3 +17,75 @@ class HttpRequest
 };
 
 #endif
+
+
+enum	ParseState
+{
+	REQUEST_LINE,		//	parsing: "GET /index.html/1.1"
+	HEADERS,			//	is parsing HTTP headers
+	BODY,				//	is parsing request body
+	COMPLETE,			//	is request fully parsed
+	ERROR_STATE			//	malformed request
+};
+
+class	HttpRequest
+{
+	public:
+		std::string	method;
+		//	GET POST DELETE
+
+		std::string	uri;
+		//	request URI/path
+		//	"/images/cat.png"
+
+		std::map<std::string, std::string> headers;
+		//	all parsed headers
+		//	headers["Host"] = "localhost:8080"
+
+		std::string body;
+		//	used for POST uploads/images
+
+		ParseState state;
+		//	curent parser state machine status
+
+		size_t contentLength;
+		//	content length extracted from Headers
+
+	public:
+		HttpRequest();
+
+		bool feed(const std::string& chunk);
+		//	used to append new raw bytes to parser
+		//	advances parsing state machine
+		//	returns true if request becomes COMPLETE
+		
+		bool isComplete() const;
+		//	returns true when parsing is finished
+};
+
+#endif
+
+
+/*
+	http request example:
+
+	POST /upload HTTP/1.1
+	Host: localhost
+	Content-Length: 5
+
+	hello
+
+	-------------------------------------
+
+	flow exmple:
+
+	socket recv()
+		↓
+	HttpRequest::feed(chunk)
+		↓
+	parse progressively
+		↓
+	request complete?
+		├── no → wait for more packets
+		└── yes → "request ready"
+*/
--- include/HttpResponse.hpp
+++ include/HttpResponse.hpp
@@ -16,3 +16,53 @@ class HttpResponse
 };
 
 #endif
+
+
+class	HttpResponse
+{
+	public:
+		int	statusCode;
+		//	HTTP status code
+		//	examples:	200, 404, etc
+
+		std::string statusMessage;
+		//	HTTP status text
+		//	examples:	"ok" or "not found"
+
+		std::map<std::string, std::string>	headers;
+		//	response headers
+		//	example:	headers["Content-Type"] = "text/html"
+
+		std::string	body;
+		//	response body sent to client
+
+		httpResponse();
+		//	initializes status code + status message
+		//	example:	200 OK, 404 Not Found
+
+		std::string	serialize() const;
+		//	converts response object into raw bytes:
+		//	// HTTP/1.0 200 OK\r\n
+		//	Header: value\r\n
+		//	\r\n
+		//	body
+	};
+
+	/*
+		httpResponse builds the server response sent to client
+
+		responsible for:
+		- status code
+		- status message
+		- headers
+		- response body
+		- converting everything into raw HTTP text (using serialize())
+	
+		example:
+		
+		Example output:
+		HTTP/1.0 200 OK
+		Content-Length: 5
+
+		hello     
+*/
