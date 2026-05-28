252060ab9373c9c9bc5f2cab9a78e15344c0c2c8
dnnyll <leal47danny@gmail.com>
2026-05-28 15:10:03 +0200

    daniefe2 - 2026-05-28 15h09m45 - added hardcoded HttpResponse + HttpRequest

--- src/HttpRequest.cpp
+++ src/HttpRequest.cpp
@@ -1,11 +1,11 @@
-#include	"HttpRequest.hpp"
-
 /*
 	This part is responsible for taking raw incoming data from the client,
 	gradually turning it into a structured HTTP request. 
 	It keeps reading chunks until the full request (headers + body) is complete.
 	In the end, it tells the system “I have everything, you can process this request now.”
 */
+
+#include	"HttpRequest.hpp"
 
 HttpRequest::HttpRequest(): state(REQUEST_LINE), contentLength(0){}
 //	parser starts at request line, there's no body length known yet
