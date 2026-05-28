79ff6e8e659bc1362710c221f9a13123bcb7d7c3
dnnyll <leal47danny@gmail.com>
2026-05-28 15:16:41 +0200

    daniefe2 - 2026-05-28 15h16m23 - added hardcoded Router.cpp

---
+++ src/Router.cpp
@@ -0,0 +1,18 @@
+/*
+	This is the decision-making core. 
+	It takes a completed request and the server configuration, 
+	then figures out what should happen (file serve, error, redirect, or CGI).
+	It builds the correct HttpResponse based on those rules and returns it to the server loop.
+*/
+
+#include	"Router.hpp"
+
+Router::Router(){}
+
+HttpResponse	Router::route(const HttpRequest&, const ServerConfig&)
+{
+	// temporary hardcoded routing
+
+	HttpResponse	response;
+	return (response);
+}
