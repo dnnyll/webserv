56f7a6285b16096ed5bac5a00b300e843679f1f5
ArchAsus <jsurian@student.42lausanne.ch>
2026-06-05 17:43:19 +0200

    jules httpresponse create class requestHandler

---
+++ inc/RequestHandler.hpp
@@ -0,0 +1,30 @@
+#ifndef		REQUESTHANDLER_HPP
+# define	REQUESTHANDLER_HPP
+
+class	RequestHandler
+{
+	private:
+		const HttpRequest	&_request;
+		const ServerBlock	&_config;
+		HttpResponse		_response;
+
+		//pointeur sur la location qui correspond a la request
+		//string du chemin officiel
+
+		//methode priver a voir si toute necessaire
+		//void	matchLocation();
+		//void	checkPermision();
+		//void	handleGet();
+		//void	handlePost();
+		//void	handleDelete();
+		//bool executeCGI();
+		//void	generateErrorPage(int statusCode);
+
+	public:
+		//constructeur pendre la reference de la request et de la config
+
+		//une fonction publique que la boucle du serverManager appelera
+		//HttpResponse process();
+}
+
+#endif
