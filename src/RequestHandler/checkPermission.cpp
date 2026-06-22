1d61592b4c53b013b53f9094c5673c7d68b102c3
ArchAsus <jsurian@student.42lausanne.ch>
2026-06-22 16:06:18 +0200

    jules respoonse start checkpermission

--- src/RequestHandler/checkPermission.cpp
+++ src/RequestHandler/checkPermission.cpp
@@ -3,5 +3,11 @@
 int RequestHandler::checkPermission()
 {
 	//avec stat check si permission ok ou non
+	//check des permissions dans cette location
+	//dossier ou fichier
+	//si dossier est ce que index
+		//si no
+		//est ce que index dans config general
+		//si oui remplace
 	return (1);	
 }
