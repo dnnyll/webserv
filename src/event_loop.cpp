627199604598540d1309ff133f30081c31afcedd
Keereslame <alexisPhilfavre@icloud.com>
2026-06-24 12:48:14 +0200

    update structure in event_loop file

--- src/event_loop.cpp
+++ src/event_loop.cpp
@@ -1,17 +1,72 @@
 #include "../inc/event_loop.hpp"
 
-int	event_loop(void)
+static void	add_client(int socket_fd, std::vector<struct pollfd> &fds, std::map<int, Client> &clients)
 {
-	struct sockaddr_in	addr = {};
-	std::vector<struct pollfd> fds;
-	std::map<int, Client> clients;
- 
+	std::cout << "New connexion" << std::endl;
+	int client_fd = accept(socket_fd, NULL, NULL);
+	if (client_fd < 0)
+	{
+		std::cerr << "accept() failed : " << strerror(errno) << std::endl;
+		return ; // <- saute à la prochaine iteration de la boucle for
+	}
+	fcntl(client_fd, F_SETFL, O_NONBLOCK); // socket non-bloquant
+	// Ajouter le client au tableau
+	struct pollfd client_pfd = {};
+	client_pfd.fd = client_fd;
+	client_pfd.events = POLLIN; // on veut lire ce qu'il envoie
+	fds.push_back(client_pfd);
+	clients[client_fd] = Client();
+	std::cout << "new client : " << client_fd << std::endl;
+}
+
+static void	remove_client(int i, std::vector<struct pollfd> &fds, std::map<int, Client> &clients)
+{
+	close(fds[i].fd);
+	clients.erase(fds[i].fd);
+	fds.erase(fds.begin() + i);
+}
+
+static bool	handle_read(int i, std::vector<struct pollfd> &fds, std::map<int, Client> &clients)
+{
+	std::cout << "Something to read" << std::endl;
+	char buffer[BUFFER_SIZE] = {};
+	int readed = read(fds[i].fd, buffer, BUFFER_SIZE);
+
+	if (readed == 0) // client déconnecté
+	{
+		std::cout << "Client déconnecté : fd=" << fds[i].fd << std::endl;
+		remove_client(i, fds, clients);
+		return (true); //client supprimé
+	}
+	clients[fds[i].fd].receive += std::string(buffer, readed);
+	//check si c'est la fin du header
+	if (clients[fds[i].fd].receive.find("\r\n\r\n") != std::string::npos)
+	{
+		// réponse prête -> on la bufferise et on active POLLOUT
+		clients[fds[i].fd].send = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+		fds[i].events = POLLIN | POLLOUT; // demander à poll de surveiller l'écriture
+	}
+	return (false); //client toujours connecté
+}
+
+static void	handle_write(int i, std::vector<struct pollfd> &fds, std::map<int, Client> &clients)
+{
+	std::cout << "Something to write" << std::endl;
+	int sent = write(fds[i].fd, clients[fds[i].fd].send.c_str(), clients[fds[i].fd].send.size());
+	if (sent > 0)
+		clients[fds[i].fd].send.erase(0, sent);
+	if (clients[fds[i].fd].send.empty())
+		fds[i].events = POLLIN;
+}
+
+static int	init_server_socket(void)
+{
 	//parsing du .config server
+	struct sockaddr_in	addr = {};
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(8080);
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
- 
-	//ça ne devrait pas changer
+
 	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (socket_fd < 0)
 	{
@@ -38,7 +93,18 @@ int	event_loop(void)
 		close(socket_fd);
 		return (1);
 	}
+	return (socket_fd);
+}
+
+int	event_loop(void)
+{
+	std::vector<struct pollfd> fds;
+	std::map<int, Client> clients;
  
+	int	socket_fd = init_server_socket();
+	if (socket_fd < 0)
+		return (1);
+
 	struct pollfd server_pfd = {};
 	server_pfd.fd = socket_fd;
 	server_pfd.events = POLLIN; // POLLIN uniquement car pour les nouvelles connexions que des entrées pas de sortie
@@ -61,76 +127,27 @@ int	event_loop(void)
 		for (int i = 0; i < (int)fds.size(); i++)
 		{
 			if (!(fds[i].revents & (POLLIN | POLLOUT | POLLERR | POLLHUP)))
-			{
-				std::cout << "nothing..." << std::endl;
 				continue; // rien sur ce fd, on passe
-			}
 			// --- Cas 1 : nouvelle connexion sur le server socket ---
 			if (fds[i].fd == socket_fd)
-			{
-				std::cout << "New connexion" << std::endl;
-				int client_fd = accept(socket_fd, NULL, NULL);
-				if (client_fd < 0)
-				{
-					std::cerr << "accept() failed : " << strerror(errno) << std::endl;
-					continue; // <- saute à la prochaine iteration de la boucle for
-				}
-				fcntl(client_fd, F_SETFL, O_NONBLOCK); // socket non-bloquant
-				// Ajouter le client au tableau
-				struct pollfd client_pfd = {};
-				client_pfd.fd = client_fd;
-				client_pfd.events = POLLIN; // on veut lire ce qu'il envoie
-				fds.push_back(client_pfd);
-				clients[client_fd] = Client();
-				std::cout << "new client : " << client_fd << std::endl;
-			}
+				add_client(socket_fd, fds, clients);
 			// --- Cas 2 : données à lire sur un client ---
 			else
 			{
 				if (fds[i].revents & (POLLERR | POLLHUP))
 				{
 					std::cout << "Erreur ou déconnexion sur fd : " << fds[i].fd << std::endl;
-					close(fds[i].fd);
-					clients.erase(fds[i].fd);
-					fds.erase(fds.begin() + i);
+					remove_client(i, fds, clients);
 					i--;
 					continue;
 				}
 				if (fds[i].revents & POLLIN)
 				{
-					std::cout << "Something to read" << std::endl;
-					char buffer[BUFFER_SIZE] = {};
-					int readed = read(fds[i].fd, buffer, BUFFER_SIZE);
-
-					if (readed == 0) // client déconnecté
-					{
-						std::cout << "Client déconnecté : fd=" << fds[i].fd << std::endl;
-						close(fds[i].fd);
-						clients.erase(fds[i].fd);
-						fds.erase(fds.begin() + i);
-						i--; // retraiter cet index
-					}
-					else
-					{
-						clients[fds[i].fd].receive += std::string(buffer, readed);
-						//check si c'est la fin du header
-						if (clients[fds[i].fd].receive.find("\r\n\r\n") != std::string::npos)
-						{
-							// réponse prête -> on la bufferise et on active POLLOUT
-							clients[fds[i].fd].send = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
-							fds[i].events = POLLIN | POLLOUT; // demander à poll de surveiller l'écriture
-						}
-					}
+					if (handle_read(i, fds, clients))
+						i--;
 				}
 				if (fds[i].revents & POLLOUT && !clients[fds[i].fd].send.empty()) //pas if/else car on peut avoir POLLIN et POLLOUT en meme temps
-				{
-					std::cout << "Something to write" << std::endl;
-					int sent = write(fds[i].fd, clients[fds[i].fd].send.c_str(), clients[fds[i].fd].send.size());
-					if (sent > 0)
-						clients[fds[i].fd].send.erase(0, sent);
-					if (clients[fds[i].fd].send.empty())
-						fds[i].events = POLLIN;
-				}
+					handle_write(i, fds, clients);
 			}
 		}
 	}
