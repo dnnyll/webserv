ed87c698ae3a6797b6ea6f2fb3c19456fc6cb4ff
ArchAsus <jsurian@student.42lausanne.ch>
2026-07-15 10:46:02 +0200

    jules router verification des methods ok aujourdhui ca roule

--- src/RequestHandler/checkMethod.cpp
+++ src/RequestHandler/checkMethod.cpp
@@ -1,13 +1,22 @@
 #include "RequestHandler.hpp"
 
+static bool	isMethodAllowed(const std::string &method, const std::vector<std::string> 
+		&methods)
+{
+	std::vector<std::string>::const_iterator	iter = methods.begin();
+
+	while (iter != methods.end())
+	{
+		if (method == *iter)
+			return true;
+		iter++;
+	}
+	return false;
+}
+
 int RequestHandler::checkMethod()
 {
-	std::vector<std::string>::const_iterator iter = _location->methods.begin(); 
-
-	while (iter != _location->methods.begin())
-	{
-		if (*iter == _request.method)
-			return (0);
-	}
-	return (1);
+	if (!isMethodAllowed(this->_request.method, this->_effconf.methods))
+		return 1;
+	return 0;
 }
