941a1e5888528850b42cc98ea696e8c17370b63e
ArchAsus <jsurian@student.42lausanne.ch>
2026-07-19 17:39:16 +0200

    jules handle get file found ne compile pas encore mais tres prochainement les amis

--- src/RequestHandler/handleGet.cpp
+++ src/RequestHandler/handleGet.cpp
@@ -2,6 +2,34 @@
 #include <fstream>
 #include <sstream>
 
+static std::string	getFileTypeFromPath(const std::string &path)
+{
+	size_t	dotPos = path.rfind('.');
+
+	if (dotPos == std::string::npos)
+		return ("");
+	return (path.substr(dotPos + 1));
+}
+
+//mime multimedia internet mail extension
+static std::string	getContentType(const std::string &path)
+{
+	std::map<std::string, std::stringt> mimeTypes;
+	mimeTypes["html"] = "text/html";
+	mimeTypes["css"] = "text/css";
+	mimeTypes["js"] = "application/javascript";
+	mimeTypes["png"] = "image/png";
+	mimeTypes["jpg"] = "image/jpeg";
+	mimeTypes["txt"] = "text/plain";
+
+	std::string fileType = getFileTypFromPath(path);
+	std::map<std::string, std::string>::const_iterator iter = mimeTypes.find(fileType);
+	if (iter == mimeTypes.end())
+		return ("application/octet-stream");
+	return (iter->second);
+}
+
+
 void	RequestHandler::handleGet()
 {
 
@@ -19,25 +47,39 @@ void	RequestHandler::handleGet()
 			std::stringstream	buffer;
 			buffer << file.rdbuf();
 			std::string	content = buffer.str();
+			
+			std::stringstream lengthStream;
+			lengthStream << content.size();
+
 			this->_response = HttpResponse::make(200, "OK");
 			this->_response.body = content;
+			this->_response.headers["Content-Length"] = length.Stream.str();
+			this->_response.headers["Content-Type"] 
+				= getContentType(this->_pathAbsolute);
+
+
+			//version	written into status line "HTTP/1.1"
+			//body
+				//populated by Router
+					//file contents, error page, CGI output
+
 			break;
 		}
-//		case DIRECTORY_LISTING:
-//			this->_response = HttpResponse::make(/*code*/, /*message*/);
-//			break;
-//		case NOT_FOUND:
-//			this->_response = HttpResponse::make(/*code*/, /*message*/);
-//			break;
-//		case FORBIDDEN:
-//			this->_response = HttpResponse::make(/*code*/, /*message*/);
-//			break;
-//		case CGI_NEEDED:
-//			//executeCGI();
-//			break;
-//		case ERROR:
-//			this->_response = HttpResponse::make(/*code*/, /*message*/);
-//			break;
+		case DIRECTORY_LISTING:
+		//	this->_response = HttpResponse::make(/*code*/, /*message*/);
+			break;
+		case NOT_FOUND:
+		//	this->_response = HttpResponse::make(/*code*/, /*message*/);
+			break;
+		case FORBIDDEN:
+		//	this->_response = HttpResponse::make(/*code*/, /*message*/);
+			break;
+		case CGI_NEEDED:
+			//executeCGI();
+			break;
+		case ERROR:
+		//	this->_response = HttpResponse::make(/*code*/, /*message*/);
+			break;
 
 	}
 }
